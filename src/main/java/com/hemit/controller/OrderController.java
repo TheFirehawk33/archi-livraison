204f90583e8ca557d8345b2a52627c85be35e8ed
TheFirehawk33 <lotfi_rais@outlook.fr>
2022-03-24 14:21:07 +0100

    Database connection

---
+++ src/main/java/com/hemit/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.hemit.controller;
+
+import com.hemit.model.Order;
+import com.hemit.repository.OrderRepository;
+import io.quarkus.mongodb.panache.PanacheQuery;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("order")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class OrderController {
+
+    @Inject
+    public OrderRepository orderRepository;
+
+    @GET
+    public Response getOrders(Order order) {
+        PanacheQuery<Order> result = orderRepository.findAll();
+        return Response.status(Response.Status.ACCEPTED).entity(result.list()).build();
+    }
+}
